3d0d94f332b0b2ecfe0f4f4c9968dea713046d67
kyung <leeseok8347@gmail.com>
2020-01-10 23:13:54 +0900

    add init, totalSupply, balanceOf

--- chaincode.go
+++ chaincode.go
@@ -5,44 +5,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
-// Chaincode is the definition of the chaincode structure.
+// ERC20Chaincode is the definition of the chaincode structure.
 type ERC20Chaincode struct {
 }
 
-// Init is called when the chaincode is instantiated by the blockchain network.
-func (cc *ERC20Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
-	fcn, params := stub.GetFunctionAndParameters()
-	fmt.Println("Init()", fcn, params)
+// ERC20Metadata is the definition of Token Meta Info
+type ERC20Metadata struct {
+	Name        string `json:"name"`
+	Symbol      string `json:"symbol"`
+	Owner       string `json:"owner"`
+	TotalSupply uint64 `json:"totalSupply"`
+}
 
+// Init is called when the chaincode is instantiated by the blockchain network.
+// params - tokenName, symbol, owner(address), amount
+func (cc *ERC20Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+	_, params := stub.GetFunctionAndParameters()
+	fmt.Println("Init called with params: ", params)
+	if len(params) != 4 {
+		return shim.Error("incorrect number of parameter")
+	}
+
+	tokenName, symbol, owner, amount := params[0], params[1], params[2], params[3]
+
+	// check amount is unsigned int
+	amountUint, err := strconv.ParseUint(string(amount), 10, 64)
+	if err != nil {
+		return shim.Error("amount must be a number or amount cannot be negative")
+	}
+
+	// tokenName & symbol & owner cannot be empty
+	if len(tokenName) == 0 || len(symbol) == 0 || len(owner) == 0 {
+		return shim.Error("tokenName or symbol or owner cannot be emtpy")
+	}
+
+	// make metadata
+	erc20 := &ERC20Metadata{Name: tokenName, Symbol: symbol, Owner: owner, TotalSupply: amountUint}
+	erc20Bytes, err := json.Marshal(erc20)
+	if err != nil {
+		return shim.Error("failed to Marshal erc20, error: " + err.Error())
+	}
+
+	// save token meta data
+	err = stub.PutState(tokenName, erc20Bytes)
+	if err != nil {
+		return shim.Error("failed to PutState, error: " + err.Error())
+	}
+
+	// save owner balance
+	err = stub.PutState(owner, []byte(amount))
+	if err != nil {
+		return shim.Error("failed to PutState, error: " + err.Error())
+	}
+
+	// response
 	return shim.Success(nil)
 }
 
 // Invoke is called as a result of an application request to run the chaincode.
 func (cc *ERC20Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	fcn, params := stub.GetFunctionAndParameters()
-
-	// GetArgs
-	args := stub.GetArgs()
-	fmt.Println("GetArgs(): ")
-	for _, arg := range args {
-		argStr := string(arg)
-		fmt.Printf("%s ", argStr)
-	}
-	fmt.Println()
-
-	// GetStringArgs()
-	stringArgs := stub.GetStringArgs()
-	fmt.Println("GetStringArgs(): ", stringArgs)
-
-	// GetArgsSlice()
-	argsSlice, _ := stub.GetArgsSlice()
-	fmt.Println("GetArgsSlice(): ", string(argsSlice))
 
 	switch fcn {
 	case "totalSupply":
@@ -70,12 +100,62 @@ func (cc *ERC20Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	}
 }
 
+// totalSuuply is query function
+// params - tokenName
+// Returns the amount of token in existence
 func (cc *ERC20Chaincode) totalSupply(stub shim.ChaincodeStubInterface, params []string) sc.Response {
-	return shim.Success([]byte("totalSupply call"))
+	// a number of params must be one
+	if len(params) != 1 {
+		return shim.Error("incorrect number of parameter")
+	}
+
+	tokenName := params[0]
+
+	// Get ERC20 Metadata
+	erc20 := ERC20Metadata{}
+	erc20Bytes, err := stub.GetState(tokenName)
+	if err != nil {
+		return shim.Error("failed to GetState, error: " + err.Error())
+	}
+	err = json.Unmarshal(erc20Bytes, &erc20)
+	if err != nil {
+		return shim.Error("failed to Unmarshal, error: " + err.Error())
+	}
+
+	// Convert TotalSupply to Bytes
+	totalSupplyBytes, err := json.Marshal(erc20.TotalSupply)
+	if err != nil {
+		return shim.Error("failed to Marshal totalSupply, error: " + err.Error())
+	}
+	fmt.Println(tokenName + "'s totalSupply is " + string(totalSupplyBytes))
+
+	return shim.Success(totalSupplyBytes)
 }
 
+// balanceOf is query function
+// params - address
+// Returns the amount of tokens owned by addresss
 func (cc *ERC20Chaincode) balanceOf(stub shim.ChaincodeStubInterface, params []string) sc.Response {
-	return shim.Success([]byte("balanceOf call !!"))
+
+	// a number of params must be one
+	if len(params) != 1 {
+		return shim.Error("incorrect number of parameters")
+	}
+
+	address := params[0]
+
+	// get Balance
+	amountBytes, err := stub.GetState(address)
+	if err != nil {
+		return shim.Error("failed to GetState, error: " + err.Error())
+	}
+
+	fmt.Println(address + "'s balance is " + string(amountBytes))
+
+	if amountBytes == nil {
+		return shim.Success([]byte("0"))
+	}
+	return shim.Success(amountBytes)
 }
 
 func (cc *ERC20Chaincode) transfer(stub shim.ChaincodeStubInterface, params []string) sc.Response {
